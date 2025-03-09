dd7f0ec0585777fe499652143e9935750781a359
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2025-03-09 23:18:59 +0530

    Added ShadCN and some images

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 	title: "Attar Malaki",
 	description:
 		"Attar Malaki is a distinguished purveyor of luxurious Arabian perfumes, harmoniously blending the rich traditions of Middle Eastern fragrance artistry with contemporary sophistication. Our mission is to offer scents that not only captivate the senses but also embody the essence of royalty and elegance.",
+	icons: {
+		icon: "/favicon.png",
+	},
 };
 
 export default function RootLayout({
@@ -25,6 +28,17 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<style>
+					{`
+            @import url("https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap");
+            body {
+              font-family: "Cairo", sans-serif;
+              font-optical-sizing: auto;
+            }
+          `}
+				</style>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				{children}
